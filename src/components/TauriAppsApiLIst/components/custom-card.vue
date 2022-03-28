5f0830a5dca23a400d2c6aed8194c8a0ab2be78d
lhf6623 <lhf6623@outlook.com>
2022-03-28 23:15:52 +0800

    feat: card 置顶、滚动到顶部

--- src/components/TauriAppsApiLIst/components/custom-card.vue
+++ src/components/TauriAppsApiLIst/components/custom-card.vue
@@ -1,18 +1,21 @@
 <template>
   <div
     class="shadow-sm m-1 px-1 pb-1 rounded-sm overflow-hidden border hover:shadow-md transition duration-300"
+    ref="cardRef"
   >
     <h1 class="card-title mb-1 border-b py-1">
       <span class="font-bold">{{ title }}</span>
       <NIcon
         :component="Link"
-        class="sticky-top hidden cursor-pointer ml-1 align-middle"
+        class="sticky-top hidden cursor-pointer ml-1 align-middle select-none"
         color="#4682BE"
+        @click="handleItemToTop"
       />
       <NIcon
         :component="ArrowBigTop"
-        class="back-top hidden cursor-pointer ml-1 align-middle"
+        class="back-top hidden cursor-pointer ml-1 align-middle select-none"
         color="#4682BE"
+        @click="handleToTop"
       />
     </h1>
     <slot></slot>
@@ -23,12 +26,36 @@
 import { NIcon } from "naive-ui";
 import { Link } from "@vicons/carbon";
 import { ArrowBigTop } from "@vicons/tabler";
+import { ref, inject, computed } from "vue";
+import { useElementBounding } from "@vueuse/core";
+
 defineProps({
   title: {
     typre: String,
     default: "",
   },
 });
+const cardRef = ref<HTMLDivElement | null>(null);
+const scrollFn = inject("scrollFn") as (
+  n: number,
+  type: "scrollBy" | "scrollTo"
+) => void;
+let { top } = useElementBounding(cardRef);
+const _top = computed(() => {
+  return top.value;
+});
+/**
+ * 把单个卡片置顶
+ */
+const handleItemToTop = () => {
+  scrollFn(_top.value, "scrollBy");
+};
+/**
+ * 回到顶部
+ */
+const handleToTop = () => {
+  scrollFn(0, "scrollTo");
+};
 </script>
 
 <style lang="scss" scoped>
--- src/components/TauriAppsApiLIst/index.vue
+++ src/components/TauriAppsApiLIst/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <NScrollbar>
+  <NScrollbar ref="scrollbarRef">
     <NDialogProvider>
       <DialogApi />
       <AppApi />
@@ -11,11 +11,16 @@
 </template>
 
 <script setup lang="ts">
+import { provide, ref } from "vue";
 import AppApi from "./app-api.vue";
 import OsApi from "./os-api.vue";
 import ProcessApi from "./process-api.vue";
 import NoticeApi from "./notice.api.vue";
 import DialogApi from "./dialog-api.vue";
 
-import { NDialogProvider, NScrollbar } from "naive-ui";
+import { NDialogProvider, NScrollbar, ScrollbarInst } from "naive-ui";
+const scrollbarRef = ref<ScrollbarInst | null>(null);
+provide("scrollFn", (top: number, type: "scrollBy" | "scrollTo") => {
+  scrollbarRef.value?.[type]({ top, behavior: "smooth" });
+});
 </script>
