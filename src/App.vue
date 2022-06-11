f871158624829929cec6860292a8a001d213e3ca
lhf6623@outlook.com <lhf6623@outlook.com>
2022-06-11 11:36:28 +0800

    test: github actions

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,5 @@
 <script setup lang="ts">
 import { NConfigProvider, NGlobalStyle } from "naive-ui";
 import XiangqiMap from "@/views/index.vue";
+console.log(`🚀 ~ XiangqiMap`, XiangqiMap);
 </script>
